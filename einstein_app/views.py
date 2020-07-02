95bf60d3d8b812e731121c228c25d97b774f5e09
liraniS <52222143+liraniS@users.noreply.github.com>
2020-07-02 19:47:50 +0300

    Added first base.html, home.html versions with menu and categories list. Modified and added urls.py and views.py accordingly

--- einstein_app/views.py
+++ einstein_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Course, Category
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'einstein_app/home.html'
+    context_object_name = 'my_lists'
+
+    def get_queryset(self):
+        categories_list = Category.objects.all()
+        courses_list = Course.objects.all()[:6]
+
+        return {'categories_list':categories_list,
+                'courses_list':courses_list,
+            }
