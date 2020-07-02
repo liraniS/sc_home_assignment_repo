7a1e85d8f5a311e434faeb95cc1d0a485a8b0358
liraniS <52222143+liraniS@users.noreply.github.com>
2020-07-02 18:48:37 +0300

    Edited models to set relationships

--- einstein_app/models.py
+++ einstein_app/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class Course(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=320, null=True)
-    category = models.ForeignKey(Category, blank=True, null=True)
-    by_user = models.ForeignKey(User, blank=True, null=True, related_name='by_user')
+    category = models.ForeignKey('Category', blank=True, null=True, related_name='course_category', on_delete=models.SET_NULL)
+    by_user = models.ForeignKey(User, blank=True, null=True, related_name='by_user', on_delete=models.SET_NULL)
     enrolled_users = models.ManyToManyField(User, blank=True, null=True, related_name='enrolled_users')
     creation_date = models.DateTimeField(auto_now_add=True, null=True)
 
@@ -16,10 +16,11 @@ class Course(models.Model):
 
 
 class Category(models.Model):
-        name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80)
     
     def __str__(self):
         return self.name
+
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=40)
